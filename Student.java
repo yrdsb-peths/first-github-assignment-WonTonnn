e5b8db7066fc5a0a733b4070535647d12bd24342
WonTonnn <116686660+WonTonnn@users.noreply.github.com>
2023-11-09 09:17:41 -0500

    Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student{
+  private String firstNm;
+  private String lastNm;
+  private String grade;
+
+  public String Student(String fN, String lN, String gd)
+  {
+    firstNm = fN;
+    lastNm = lN;
+    grade = gd
+  }
+
+}
